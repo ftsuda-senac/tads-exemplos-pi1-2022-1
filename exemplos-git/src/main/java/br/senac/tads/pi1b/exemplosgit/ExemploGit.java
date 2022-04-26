f39d71b60d8b02e4ee439d93b7167f4d40d812bb
ftsuda-senac <ftsuda.senac@gmail.com>
2022-04-25 22:04:53 -0300

    Criação de novo projeto Java

---
+++ exemplos-git/src/main/java/br/senac/tads/pi1b/exemplosgit/ExemploGit.java
@@ -0,0 +1,12 @@
+package br.senac.tads.pi1b.exemplosgit;
+
+public class ExemploGit {
+
+    public static void main(String[] args) {
+        
+        int soma = 1 + 1;
+        System.out.println("Resultado soma: " + soma);
+        
+    }
+    
+}
